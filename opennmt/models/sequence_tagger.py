8f14bbe03a88107bf381743623283012cfd8aae7
Jordi Mas <jmas@softcatala.org>
2024-08-20 23:39:01 +0200

    Sequence tagger

--- opennmt/models/sequence_tagger.py
+++ opennmt/models/sequence_tagger.py
@@ -2,10 +2,10 @@
 
 import numpy as np
 import tensorflow as tf
+import opennmt.tfa as tfa
 
 from opennmt import inputters
 from opennmt.models.model import Model
-from opennmt.tfa.text import crf_decode, crf_log_likelihood
 from opennmt.utils.losses import cross_entropy_sequence_loss
 
 
@@ -52,7 +52,7 @@ class SequenceTagger(Model):
         logits = self.output_layer(outputs)
         if not training:
             if self.crf_decoding:
-                tags_id, _ = crf_decode(logits, self.transition_params, length)
+                tags_id, _ = tfa.text.crf_decode(logits, self.transition_params, length)
                 tags_id = tf.cast(tags_id, tf.int64)
             else:
                 tags_prob = tf.nn.softmax(logits)
@@ -68,7 +68,7 @@ class SequenceTagger(Model):
 
     def compute_loss(self, outputs, labels, training=True):
         if self.crf_decoding:
-            log_likelihood, _ = crf_log_likelihood(
+            log_likelihood, _ = tfa.text.crf_log_likelihood(
                 outputs,
                 tf.cast(labels["tags_id"], tf.int32),
                 labels["length"],

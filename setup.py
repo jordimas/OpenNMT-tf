87b36c326bec26c3903f764260adc3d0d0f2ce9a
Jordi Mas <jmas@softcatala.org>
2024-08-20 16:02:49 +0200

    Fix 2

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
         "pyyaml>=5.3,<7",
         "rouge>=1.0,<2",
         "sacrebleu>=1.5.0,<3",
-        "typeguard"
+        "typeguard",
     ],
     extras_require={
         "tensorflow": [

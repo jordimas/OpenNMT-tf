d1da8efafce8f11735a370eaa90ce716e8334d25
Jordi Mas <jmas@softcatala.org>
2024-08-21 20:11:19 +0200

    Significant cleanup

--- opennmt/tfa/seq2seq/__init__.py
+++ opennmt/tfa/seq2seq/__init__.py
@@ -24,17 +24,12 @@ from opennmt.tfa.seq2seq.attention_wrapper import (
     monotonic_attention,
     safe_cumprod,
 )
-from opennmt.tfa.seq2seq.basic_decoder import BasicDecoder, BasicDecoderOutput
 from opennmt.tfa.seq2seq.beam_search_decoder import tile_batch
 from opennmt.tfa.seq2seq.decoder import BaseDecoder, Decoder, dynamic_decode
 from opennmt.tfa.seq2seq.loss import SequenceLoss, sequence_loss
 from opennmt.tfa.seq2seq.sampler import (
     CustomSampler,
-    GreedyEmbeddingSampler,
     InferenceSampler,
-    SampleEmbeddingSampler,
     Sampler,
-    ScheduledEmbeddingTrainingSampler,
-    ScheduledOutputTrainingSampler,
     TrainingSampler,
 )
